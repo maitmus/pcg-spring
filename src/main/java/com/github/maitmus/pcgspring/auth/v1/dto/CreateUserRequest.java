57ffd6ffdef0075cbf964e0ce89063e9580e6fe0
MaiT <maitmus@gmail.com>
2025-08-01 22:23:32 +0900

    fix: minor

--- src/main/java/com/github/maitmus/pcgspring/auth/v1/dto/CreateUserRequest.java
+++ src/main/java/com/github/maitmus/pcgspring/auth/v1/dto/CreateUserRequest.java
@@ -1,5 +1,6 @@
 package com.github.maitmus.pcgspring.auth.v1.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -26,6 +27,7 @@ public class CreateUserRequest extends LoginRequest {
     @NotNull
     private LocalDate birth;
 
+    @Schema(hidden = true)
     public void setHashedPassword(String hashedPassword) {
         this.password = hashedPassword;
     }
